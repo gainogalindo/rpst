0f2767780ac10482b4510c530d88b097a87e0858
Guilherme <guilherme.alf@gmail.com>
2026-09-04 21:32:55 -0300

    Removed IntelliJ files

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("jBPT Library: Refined Process Structure Tree (RPST)");
+        System.out.println("jBPT Library: Refined Process Structure Tree (RPST). Compiled for Java 1.8");
     }
 }
